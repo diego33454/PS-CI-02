2817c8fc435a17ec042437c40b12c9419daf99d8
diego33454 <164555877+diego33454@users.noreply.github.com>
2025-02-28 16:26:04 +0100

    4 commit

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log(`Servidor web en el puerto ${PORT}.`);
 });
+
+server.closeAll = () => {
+   app.con.end()
+   server.close()
+}
+
+module.exports = server
